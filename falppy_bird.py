13e51d768be541f69516fe495e27580786877a72
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-13 23:07:25 +0200

    "adding main method for test"

--- falppy_bird.py
+++ falppy_bird.py
@@ -74,3 +74,32 @@ class Bird:
 
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
+    
+    def draw_window(win, bird):
+        win.blit(BG_IMGS[0], (0, 0))
+        bird.draw(win)
+        pygame.display.update()
+
+
+
+
+
+
+
+    def main():
+        bird = Bird(200,200)
+        win = pygame.display.set_mode(( WIN_WIDTH, WIN_HEIGHT))
+        clock = pygame.time.Clock()
+
+        run = True
+        while run:
+            clock.tick(30)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            bird.move()
+            bird.draw_window(win, bird)
+
+        pygame.quit()
+        quit()
+    
