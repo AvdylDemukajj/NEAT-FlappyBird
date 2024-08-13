88be69cb736e7226b9ccb1550921ce672369c66d
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-13 22:48:26 +0200

    "jump method"

--- falppy_bird.py
+++ falppy_bird.py
@@ -28,3 +28,8 @@ class Bird:
         self.img_count = 0
         self.img = self.IMGS[0]
 
+    def jump(self):
+        self.vel = -10.5
+        self.tick_count = 0
+        self.height = self.y
+
