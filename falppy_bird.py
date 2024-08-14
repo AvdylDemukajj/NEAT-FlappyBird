b06226c3240c8521ee7f7a875a30076deab4f658
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-08-14 12:25:36 +0200

    "run method for neat algo"

--- falppy_bird.py
+++ falppy_bird.py
@@ -213,4 +213,21 @@ def main():
     quit()
 
 main()
-    
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    p = neat.Population(config)
+
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+
+    winner = p.run(main, 50)
+
+if __name__ == "__main__":
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir,"config-feedforward.txt")
+    run(config_path)    
